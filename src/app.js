82c222c6791e6b62781c80c8a3d6afc0394deb1f
Tommy <biometricmc@gmail.com>
2020-09-29 04:09:05 +1000

    Added logging & app listener

--- src/app.js
+++ src/app.js
@@ -6,3 +6,10 @@ const expressLayouts = require('express-ejs-layouts');
 const morgan = require('morgan');
 
 const app = express();
+
+// Logging
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
+
+app.listen(config.port, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${config.port}`));
