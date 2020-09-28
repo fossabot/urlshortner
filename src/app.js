73ff3d7c87085583a549db0a900f1567b38f5571
Tommy <biometricmc@gmail.com>
2020-09-29 04:05:50 +1000

    Added deps & initialised express

---
+++ src/app.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const config = require('../config/config');
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const morgan = require('morgan');
+
+const app = express();
