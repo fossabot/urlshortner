e3b2f1d07db74420ec575ac31cb155a5e0be4416
Tommy <biometricmc@gmail.com>
2020-09-29 04:10:53 +1000

    Set view engine & static folder

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const config = require('../config/config');
+const { join } = require('path');
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const morgan = require('morgan');
@@ -9,7 +10,15 @@ const app = express();
 
 // Logging
 if (process.env.NODE_ENV === 'development') {
-    app.use(morgan('dev'));
+	app.use(morgan('dev'));
 }
+
+// View Engine
+app.use(expressLayouts);
+app.set('layout', 'layouts/main');
+app.set('view engine', 'ejs');
+
+// Static Folder
+app.use(express.static(join(__dirname, '../public')));
 
 app.listen(config.port, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${config.port}`));
