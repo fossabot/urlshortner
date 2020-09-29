108bda7e47df446ffb15ab004968816ef8b2744e
Tommy <biometricmc@gmail.com>
2020-09-30 05:18:31 +1000

    Added methodOverride middleware

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const { join } = require('path');
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const morgan = require('morgan');
+const methodOverride = require('method-override');
 const connectDB = require('../database/db');
 
 // Connect to MongoDB
@@ -21,6 +22,17 @@ if (process.env.NODE_ENV === 'development') {
 app.use(express.urlencoded({
 	extended: false,
 }));
+app.use(express.json());
+
+// Method Override
+app.use(methodOverride((req, res) => {
+	if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+		// look in urlencoded POST bodies and delete it
+		const method = req.body._method;
+		delete req.body._method;
+		return method;
+	}
+}))
 
 // View Engine
 app.use(expressLayouts);
