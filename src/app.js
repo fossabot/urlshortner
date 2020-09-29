70b90e6eea0c7711cfc1d02ccc2e41502a56d93d
Tommy <biometricmc@gmail.com>
2020-09-29 20:20:51 +1000

    Added express json

--- src/app.js
+++ src/app.js
@@ -17,6 +17,10 @@ if (process.env.NODE_ENV === 'development') {
 	app.use(morgan('dev'));
 }
 
+app.use(express.json({
+	extended: false,
+}));
+
 // View Engine
 app.use(expressLayouts);
 app.set('layout', 'layouts/main');
