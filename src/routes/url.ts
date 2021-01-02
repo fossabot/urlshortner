5e1080dd0a6641b6240419b3d2a7ed1e20b8181a
Tommy <biometricmc@gmail.com>
2021-01-02 23:39:23 +1100

    Generate urlCode & check if longUrl is valid & check if exists else create entry

--- src/routes/url.ts
+++ src/routes/url.ts
@@ -10,11 +10,46 @@ import Url from "../models/Url";
 // @desc    Create short Url
 router.post("/shorten", async (req:Request, res:Response) => {
    const { longUrl } = req.body;
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
    const baseUrl:string = process.env.BASE_URL!;
 
    // Check if baseUrl is valid
     if (!isUri(baseUrl)) {
         return res.status(400).json("Invalid base url");
+    }
+
+    // Create url code
+    const urlCode = generate();
+
+    // Check if longUrl is valid
+    if (isUri(longUrl)) {
+        try {
+            let url = await Url.findOne({ longUrl });
+
+            if (url) {
+                res.sendStatus(409).json("Resource already exists");
+            }
+            else {
+                const shortUrl = baseUrl + "/" + urlCode;
+
+                url = new Url({
+                    longUrl,
+                    shortUrl,
+                    urlCode,
+                });
+
+                await url.save();
+
+                res.sendStatus(201).json(`${shortUrl}`);
+            }
+        }
+        catch (err) {
+            console.error(err);
+            return res.status(400).json("Invalid long url");
+        }
+    }
+    else {
+        return res.status(400).json("Invalid long url");
     }
 });
 
